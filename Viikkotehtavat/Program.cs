0350879faabb0acb57bdbcf3e1f93ed49b42a6c5
EsaSalmik <esa.salmikangas@jamk.fi>
2017-02-07 17:15:01 +0200

    Vehicle&Tyres demo

--- Viikkotehtavat/Program.cs
+++ Viikkotehtavat/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using JAMK.IT;
 
 namespace Viikkotehtavat
 {
@@ -10,7 +11,8 @@ namespace Viikkotehtavat
   {
     static void Main(string[] args)
     {
-      Tehtava1();
+      //Tehtava1();
+      Lab05Teht01();
     }
 
     static void Tehtava1()
@@ -37,6 +39,26 @@ namespace Viikkotehtavat
       }
       //tulostus käyttäjälle
       Console.WriteLine(retval);
+    }
+
+    static void Lab05Teht01()
+    {
+      //Prorammed and tested by Esa 7.2.2017
+      //testataan Tyre ja Vehicle luokkia
+      //luodaan ensimmäinen ajoneuvo
+      Vehicle auto = new JAMK.IT.Vehicle();
+      auto.Brand = "BMW";
+      auto.Model = "320";
+      //ostetaan parit renkaat
+      Tyre rinkula = new Tyre();
+      rinkula.Branch = "Hankook";
+      rinkula.Size = "205/55R16";
+      //laitetaan bemariin taakse renkaat
+      auto.AddTyre(rinkula);
+      auto.AddTyre(rinkula);
+
+      //näytetääs auton tiedot
+      Console.WriteLine("Autossasi {0} on seuraavat tiedot: {1}", auto.Brand, auto.ToString());
     }
   }
 }
